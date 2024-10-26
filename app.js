d353173804bf341b4a492e8169b9e4565bacd62d
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2024-10-26 15:56:25 +0530

    fixes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import dbConnection from "./src/db/dbConnection.js";
 import userRouter from "./src/routes/userRouter.js";
-import errorMiddleware from "./src/middlewares/Error.js";
+import errorMiddleware from "./src/middlewares/allError.js";
 
 const app = express();
 dotenv.config();
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(errorMiddleware);
+app.use(errorMiddleware());
 
 dbConnection();
 
