cb8c8d31953ba46aea03a322c453d8693d0e28bf
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2024-11-24 11:41:46 +0530

    cors

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
